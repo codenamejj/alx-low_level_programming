6b1c0d0f9e56ef7881e8dbae5ae82246a268aac7
codenamejj <jmtagare@gmail.com>
2022-10-16 11:58:24 +0000

    Updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
-/**
-*main = void
-*main return = 0 (success)
-*/
-int main(void)
+
+int main(void)/**entry point */
 
 {
 	char c;
@@ -17,12 +14,12 @@ int main(void)
 
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(lt));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(llt));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c)); /** evaluates to 1 byte*/
+	printf("Size of an int: %lu byte(s)\n", sizeof(i)); /** evaluates to 4 byte*/
+	printf("Size of a long int: %lu byte(s)\n", sizeof(lt)); /** evaluates to 4 byte*/
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llt)); /** evaluates to 8 byte*/
+	printf("Size of a float: %lu byte(s)\n", sizeof(f)); /** evaluates to 4 byte*/
 
-	return (0);
+	return (0); /**evalutes to 0 (success)*/
 
 }
