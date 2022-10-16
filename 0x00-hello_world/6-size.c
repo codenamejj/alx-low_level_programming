22b7eddf5b0cbbc126d906233a3616292ec85e90
codenamejj <jmtagare@gmail.com>
2022-10-16 12:01:29 +0000

    Latest Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 
-int main(void)/**entry point */
+int main(void)
+/**entry point */
 
 {
 	char c;
@@ -14,12 +15,18 @@ int main(void)/**entry point */
 
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c)); /** evaluates to 1 byte*/
-	printf("Size of an int: %lu byte(s)\n", sizeof(i)); /** evaluates to 4 byte*/
-	printf("Size of a long int: %lu byte(s)\n", sizeof(lt)); /** evaluates to 4 byte*/
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(llt)); /** evaluates to 8 byte*/
-	printf("Size of a float: %lu byte(s)\n", sizeof(f)); /** evaluates to 4 byte*/
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+/** evaluates to 1 byte*/
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+/** evaluates to 4 byte*/
+	printf("Size of a long int: %lu byte(s)\n", sizeof(lt));
+/** evaluates to 4 byte*/
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llt));
+/** evaluates to 8 byte*/
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+/** evaluates to 4 byte*/
 
-	return (0); /**evalutes to 0 (success)*/
+	return (0);
+/**evalutes to 0 (success)*/
 
 }
