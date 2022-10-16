ef8d1d7ac294517823a541aa2cf895b86a0fce8c
codenamejj <jmtagare@gmail.com>
2022-10-16 12:02:55 +0000

    Latest Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,15 +18,15 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 /** evaluates to 1 byte*/
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-/** evaluates to 4 byte*/
+/** evaluates to 4 bytes*/
 	printf("Size of a long int: %lu byte(s)\n", sizeof(lt));
-/** evaluates to 4 byte*/
+/** evaluates to 4 bytes*/
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(llt));
-/** evaluates to 8 byte*/
+/** evaluates to 8 bytes*/
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-/** evaluates to 4 byte*/
+/** evaluates to 4 bytes*/
 
 	return (0);
-/**evalutes to 0 (success)*/
+/**return 0 (success)*/
 
 }
