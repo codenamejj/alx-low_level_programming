4c4b59a6e1e26cffcedefe986a43b2060f0ffa96
codenamejj <jmtagare@gmail.com>
2022-10-17 00:02:22 +0000

    Base 10 integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - .print base 10 num
+*
+* Return: 0 always
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		printf("%i\n", x);
+	}
+
+	return (0);
+}
+
