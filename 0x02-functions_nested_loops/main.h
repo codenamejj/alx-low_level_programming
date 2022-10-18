d47f5b9d221f37aadb52f65e6089ba6abd5582b5
codenamejj <jmtagare@gmail.com>
2022-10-18 23:33:58 +0000

    Personal Library

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,10 @@
-int _putchar(char c);
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/* my personal library*/
+
+
+int _putchar(char c); /* printing characters */
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _abs(int);
@@ -10,4 +16,4 @@ void print_to_98(int n);
 int _islower(int c);
 
 
-
+#endif
