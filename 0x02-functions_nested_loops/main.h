399242c7b72261ae48f1d607ff020f51c4ecd9ba
codenamejj <jmtagare@gmail.com>
2022-10-19 13:15:56 +0000

    main.h file update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,11 +15,10 @@
 /* PROTOTYPES */
 
 
-
-
-
 int _putchar(char c); /* printing characters */
 void print_alphabet(void);
+int _isalpha(int c);
+int print_sign(int n);
 void print_alphabet_x10(void);
 int _abs(int);
 int print_last_digit(int);
