d44b3bd478600a857582a5c6ae79679b3fc9e117
codenamejj <jmtagare@gmail.com>
2022-10-19 23:36:03 +0000

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - print natural num
+ *@n: num
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	for (n >= 1; n >= 98; n++)
+	{
+		_putchar(n);
+	}
+
+	return;
+}
+
