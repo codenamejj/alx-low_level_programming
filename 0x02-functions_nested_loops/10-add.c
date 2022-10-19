65e04f59ec5c10f560bd27493e1a9ce9cfea97a5
codenamejj <jmtagare@gmail.com>
2022-10-19 23:23:52 +0000

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add- print sum
+ *@i: integer variable1
+ *@x: integer variable2
+ * Return: result always.
+ */
+int add(int i, int x)
+{
+	int result;
+
+	result = i + x;
+
+	return (result);
+}
