6199b863be49527b172bfb14efc205fbcb66fc30
codenamejj <jmtagare@gmail.com>
2022-10-19 22:36:41 +0000

    counting

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - print minutes
+ *
+ * Return: nothing 
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+
+		{
+	
+		_putchar(hour);
+		_putchar(':');
+		_putchar(minute);
+		_putcher('\n');
+
+		}
+	}
+
+	return;
+
+
+}
+
