a6838304560dcec8d4ad16eb8cc4552f19cddff8
codenamejj <jmtagare@gmail.com>
2022-10-19 23:06:45 +0000

    final update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 		{
 
 		_putchar(hour / 10 + 48);
-		_putchar(hour % 10 + 48):
+		_putchar(hour % 10 + 48);
 		_putchar(':');
 		_putchar(minute / 10 + 48);
 		_putchar(minute % 10 + 48);
