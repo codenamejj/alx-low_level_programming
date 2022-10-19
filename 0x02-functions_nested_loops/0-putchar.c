dbd04e0799472a8b7f5c0b51ea9b1e2c6b7dcf71
codenamejj <jmtagare@gmail.com>
2022-10-19 00:23:50 +0000

    update fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -14,7 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	
+
 
 	_putchar('\n');
 	return (0);
