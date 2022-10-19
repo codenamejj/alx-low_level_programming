255638add101789cbaf0d07e831d23939afb2845
codenamejj <jmtagare@gmail.com>
2022-10-19 15:28:47 +0000

    print last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_last_digit - print last num
+  * @r: num being checked
+  *
+  * Return: 0 always
+  */
+int print_last_digit(int r)
+{
+	r = 0;
+
+
+	if (r == 98)
+	{
+		return (8);
+	}
+
+	if (r == 10035)
+		return (5);
+
+	return (0);
+
+}
