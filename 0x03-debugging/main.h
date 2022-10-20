416f1f950a4bcecf113220ac6fc420441a134d13
codenamejj <jmtagare@gmail.com>
2022-10-20 14:40:10 +0000

    New dir

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN.H
+#define _MAIN.H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int convert_day(int month, int day);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+
+#endif /* MAIN_H */
