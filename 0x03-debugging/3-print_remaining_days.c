ac0157541d400f9ed5a477b96c370d8f6701338e
codenamejj <jmtagare@gmail.com>
2022-10-21 11:10:40 +0000

    Bug fixed

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 /* 2000 is a special leap year and is divisible by 100 */
 	{
 	if (month >= 2 && day >= 60)
@@ -21,6 +21,20 @@ void print_remaining_days(int month, int day, int year)
 
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
+
+	}
+
+	if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+	{
+	if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
+	
+	{
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 366 - day);
+	}
 	}
 	else
 	{
