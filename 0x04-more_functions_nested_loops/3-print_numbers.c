e95942f8a2cf5a8239765859d7657037c7fccb40
codenamejj <jmtagare@gmail.com>
2022-10-22 00:41:51 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * print_numbers - print numbers
+ *
+ * Return: nothing.
+ */
+void print_numbers(void)
+{
+	char c;
+	c = 0;
+	
+	if (c > 47 && c < 58)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return;
+
+}
