009d27d4facc05146f063a4d4ccd37a46fa52b9a
codenamejj <jmtagare@gmail.com>
2022-10-22 02:36:26 +0000

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,18 @@
  */
 void more_numbers(void)
 {
+		int x, j;
 
-	int x, j;
-
-	for (x = 0; x < 11; x++)
-	{
-		for (j = 0; j < 15; j++)
-			{		
+		for (x = 0; x < 11; x++)
+		{
+			for (j = 0; j < 15; j++)
+			{
 				_putchar(j);
 			}
-		
-	
+
 		_putchar(x);
 
-	_putchar('\n');
-	}
+		_putchar('\n');
+		}
 
 }
