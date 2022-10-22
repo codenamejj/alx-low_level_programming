5b5b5495221e94d5a5c1ce272b5b9c01e749b5f0
codenamejj <jmtagare@gmail.com>
2022-10-22 00:09:50 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+/**
+ * _isdigit - check the digits
+ * @c: value being checked
+ * Return: 1 or 0.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+	return (0);
+}
