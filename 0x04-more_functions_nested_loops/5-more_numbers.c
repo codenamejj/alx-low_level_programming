766df40a2228e136386f38aa827d968963641aa5
codenamejj <jmtagare@gmail.com>
2022-10-22 02:31:22 +0000

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ *
+ * Return: nothing.
+ */
+void more_numbers(void)
+{
+
+	int x, j;
+
+	for (x = 0; x < 11; x++)
+	{
+		for (j = 0; j < 15; j++)
+			{		
+				_putchar(j);
+			}
+		
+	
+		_putchar(x);
+
+	_putchar('\n');
+	}
+
+}
