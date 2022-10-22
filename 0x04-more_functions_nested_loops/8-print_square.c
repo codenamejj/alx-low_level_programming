1c879bc293e083f6939146bee3c7e9a09a0f2143
codenamejj <jmtagare@gmail.com>
2022-10-22 03:23:05 +0000

    new

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - check the code
+ *
+ * @size: size of square
+ *
+ * Return: nothing.
+ */
+void print_square(int size)
+{
+
+	if (size == 0 || size < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+
+}
+
+
+
+
