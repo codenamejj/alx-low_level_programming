7d090c488265776f3f4b3fd2ea40662c96eb0c3e
codenamejj <jmtagare@gmail.com>
2022-10-23 19:18:50 +0000

    Putchar with comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - print possible combinations
+  *
+  * Return: 0 always
+  */
+int main(void)
+{
+	int x = 0;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
