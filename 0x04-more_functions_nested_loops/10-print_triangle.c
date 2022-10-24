49a8a037616836b5846255685ce8241344a8af0e
codenamejj <jmtagare@gmail.com>
2022-10-24 21:16:12 +0000

    reverse triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,9 @@ void print_triangle(int size)
 {
 	int x, y;
 
-	for (x = 0; x <= size; x++)
+	if (size <= 0)
+		_putchar('\n');
+	for (x = 0; x < size; x++)
 	{
 		for (y = x; y <= size; y++)
 		{
@@ -19,11 +21,7 @@ void print_triangle(int size)
 
 		for (y = 0; y <= x; y++)
 		{
-			_putchar('#');
-
-			if (size <= 0)
-			_putchar('\n');
-
+				_putchar('#');
 		}
 		_putchar('\n');
 	}
