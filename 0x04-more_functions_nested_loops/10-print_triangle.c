b7c8b70510bc6edc78dd59572dcef7a5b24d5831
codenamejj <jmtagare@gmail.com>
2022-10-24 19:18:52 +0000

    mirrored triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ *
+ * @size: size of triangle
+ *
+ * Return: nothing.
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	for (x = 0; x <= size; x++)
+	{
+		for (y= x; y < size; y++)
+		{
+			_putchar(' ');
+		}
+	
+		for (y = 0; y <= x; y++)
+		{	
+			_putchar('#');
+
+		if (size <= 0)
+				_putchar('\n');
+
+		}
+		_putchar('\n');
+	}
+
+}
