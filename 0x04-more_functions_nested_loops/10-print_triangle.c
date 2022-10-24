940656381a68aac9e7d8532345298c26438fb587
codenamejj <jmtagare@gmail.com>
2022-10-24 19:53:05 +0000

    updated

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,19 +10,19 @@ void print_triangle(int size)
 {
 	int x, y;
 
-	for (x = 1; x <= size; x++)
+	for (x = 0; x < size; x++)
 	{
-		for (y = x; y < size; y++)
+		for (y= x; y <= size; y++)
 		{
 			_putchar(' ');
 		}
-
-		for (y = 1; y <= x; y++)
-		{
+	
+		for (y = 0; y <= x; y++)
+		{	
 			_putchar('#');
 
 			if (size <= 0)
-				_putchar('\n');
+			_putchar('\n');
 
 		}
 		_putchar('\n');
