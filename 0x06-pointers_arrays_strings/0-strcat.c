130c707b6a647dbc52bd18715d79044ecd796c94
codenamejj <jmtagare@gmail.com>
2022-10-29 00:13:31 +0000

    concatenates

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * *_strcat - concatenates two strings
+  * @*dest:
+  * @*src:
+  *
+  * Return: Always 0.
+  */
+char *_strcat(char *dest, char *src)
+{
+	int s, i;
+
+	s = 0;
+	while (dest[s] != 0)
+		s++;
+	for (i = 0; src[i] != 0; i++)
+		dest[s + i] = src[i];
+	dest[s + i] = 0;
+	return (dest);
+
+	return (0);
+}
