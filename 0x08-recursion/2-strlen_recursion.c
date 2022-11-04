653fb83a4a69a2e93c81410bd3b770a78a11aef4
codenamejj <jmtagare@gmail.com>
2022-11-04 17:24:15 +0000

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
-	if (*s != '\0')
-	{
-		s++;
-	length++;
-	}
-	return (length);
-
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) +1);
+	return (0);
 }
