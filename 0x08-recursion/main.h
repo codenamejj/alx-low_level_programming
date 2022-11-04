a5d4091615b4e8e1053f8ba6db0831e278f1b04a
codenamejj <jmtagare@gmail.com>
2022-11-04 15:01:15 +0000

    prototypes

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/* standard library */
+
+#include <stdio.h>
+
+/* PROTOTYPES */
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+
+#endif /* main.h */
+
+
+
