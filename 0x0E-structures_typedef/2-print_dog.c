c6e583823344a164c932d97489691cc4c1c06c77
codenamejj <jmtagare@gmail.com>
2022-11-15 22:06:02 +0000

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,12 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name == NULL)
-			printf("(nil)");
 		if (d->owner == NULL)
-			printf("(nil)");
+			d->owner = "(nil)";
+
+		if (d->name == NULL)
+			printf("Name: (nil)");
+
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 	else
