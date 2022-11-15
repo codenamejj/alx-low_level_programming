52761906516b01feadb55ef4531861af6399b49f
codenamejj <jmtagare@gmail.com>
2022-11-15 14:20:39 +0000

    Directory

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+  * struct dog - struct
+  * @name: name
+  * @age: age
+  * @owner: owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
