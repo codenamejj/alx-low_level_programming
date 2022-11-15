6290934543172d60239108ff0fc866a5e89c4b7d
codenamejj <jmtagare@gmail.com>
2022-11-15 16:33:35 +0000

    New update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of type struct dogi
  * @d: struct
@@ -12,8 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
-	d->owner = owner;
-
+	d->owner = owner;	
 }
 
 
