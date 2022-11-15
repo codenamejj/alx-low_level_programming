45e3b402eacbbc3f6c832b087c5581d3eeb671fa
codenamejj <jmtagare@gmail.com>
2022-11-15 15:18:17 +0000

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,4 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/* PROTOTYPES */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-unsigned int _strlen(char *s);
-char *_strcpy(char *s);
 #endif
