73be4743a15e18d078f5c5c0d3e0500ae1432b9a
codenamejj <jmtagare@gmail.com>
2022-11-15 22:07:03 +0000

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 			d->owner = "(nil)";
 
 		if (d->name == NULL)
-			printf("Name: (nil)");
+			d->name = "Name: (nil)";
 
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
