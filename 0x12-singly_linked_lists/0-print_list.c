4828e24d76ee4c9ff66c17bd1c3e3d8fe135c187
codenamejj <jmtagare@gmail.com>
2022-11-28 23:02:23 +0000

    initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+/**
+  * print_list - print list
+  * @h: points to constant
+  * Return: 0
+  *
+  */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	while (str != NULL)
+	{
+		str++;
+		count++;
+	}
+	return (count);
+
+	printf("[%d] %s\n", count, str);
+	
+	return (0);
+}
