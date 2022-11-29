6dded474e0c56a734a1338cd256d4f5c24bac786
codenamejj <jmtagare@gmail.com>
2022-11-29 00:15:38 +0000

    only code approved. previous commit seemed to work similarly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,15 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
 	while (h)
 	{	
-		if (h->str != NULL)
+		if (h->str)
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
-		if (h->str == NULL)
+		else
 		{
 			printf("[0] (nil)\n");
 		}
