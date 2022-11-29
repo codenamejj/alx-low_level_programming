6444ccbf72de7c35985fbc0a24cee72ea40f3fd5
codenamejj <jmtagare@gmail.com>
2022-11-29 00:07:34 +0000

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,22 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	int count = 0;
 
-	while (h != NULL)
-	{
-		printf("[%d] %s\n", h->len, h->str);
+	while (h)
+	{	
+		if (h != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		if (h == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		count++;
 		h = h->next;
-		nodes++;
-	}
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-		h = h->next;
-	}
 	
-	return (nodes);
+	}
+	return (count);
 }
 
