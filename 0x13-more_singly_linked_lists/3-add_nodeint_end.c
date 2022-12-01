93b4a4fdbc8c19cc66e9d2f284d44820ae239633
codenamejj <jmtagare@gmail.com>
2022-12-01 16:43:38 +0000

    only betty fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
-  * add_noneint_end - adds a new node at the end of a listint_t list
+  * add_nodeint_end - adds a new node at the end of a listint_t list
   * @head: pointer to pointer to struct
   *
-  * @n: constant 
-  *
+  * @n: constant
+  * Return: the address of the new element, or NULL if it failed
   */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
