4f2af7b031b9b09ea09baabfc57d52b3f9c116d7
codenamejj <jmtagare@gmail.com>
2022-12-01 09:52:11 +0000

    Init commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,13 +5,20 @@
   * @h: constant pointer to struct
   * Return: elements
   */
-
 size_t print_listint(const listint_t *h)
 {
 	unsigned int count;
 
 	while (h)
 	{
+		if (h->n)
+		{
+			printf("%d\n", h->n);
+		}
+		if (!h->n)
+		{
+			printf("Nil\n");
+		}
 		count++;
 		h = h->next;
 	}
