75310d44659a4c553946d0615513a47bd028769b
codenamejj <jmtagare@gmail.com>
2022-12-01 09:48:40 +0000

    Updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  * print_listint - prints all the elements of a listint_t list
+  *
+  * @h: constant pointer to struct
+  * Return: elements
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+
