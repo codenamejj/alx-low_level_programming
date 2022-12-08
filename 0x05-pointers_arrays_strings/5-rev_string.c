04c1108e8d8b289f04def2d2b2c9a813c780d5a9
codenamejj <jmtagare@gmail.com>
2022-12-08 13:19:36 +0000

    not fixed yet

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *
+  *
+  *
+  *
+  */
+void rev_string(char *s)
+{
+	int count = 10;
+	int index;
+
+
+	while (s[count])
+	{
+		
+		for (index = 0; index < count; index++)
+		{
+			if (count >= 0)
+				count++;
+		}
+	}
+	while (index)
+	{
+		for (index = count - 1; index >=0; index --)
+			if (count != 0)
+				count--;
+	}
+}
