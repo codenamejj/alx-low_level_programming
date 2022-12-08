ffc9553de09cdcfc97f07258e56f610c005327c0
codenamejj <jmtagare@gmail.com>
2022-12-08 11:38:02 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * print_rev - prints in reverse
+  * @s: pointer to character
+  *
+  * Return: nothing
+  */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	while (count)
+	{
+		_putchar(*s);
+		s--;
+		count = count - 1;
+	}
+
+	_putchar('\n');
+}
