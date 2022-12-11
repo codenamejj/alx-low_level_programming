af9b78e0dd912b585b598d34f220c5f5931bfdc6
codenamejj <jmtagare@gmail.com>
2022-12-11 22:50:32 +0000

    not completed

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "main.h"
+/**
+  * main - checks code
+  * @argc: count
+  * @argv: value
+  * Return: 0 if success or 1 if error
+  */
+int main(int argc, int argv[])
+{
+	int fd;
+	char usage[] = "Usage: cp file_from file_to";
+	char error[] = "rror: Can't read from file NAME_OF_THE_FILE";
+
+	fd = open("file_from", O_RDWR | O_APPEND);
+	if (fd == -1)
+	{
+		_putchar(usage);
+		_putchar('\n');
+		exit(97);
+	}
+	if (!file_from | !file_from == NULL)
+	{
+		_putchar(error);
+		_putchar('\n');
+		exit(98);
+	}
+
+
+		
+	return (0);
+
+}
