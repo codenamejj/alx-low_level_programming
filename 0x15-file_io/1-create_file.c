3b19143de0ca99cebbd302563974b9a579a26bc1
codenamejj <jmtagare@gmail.com>
2022-12-11 21:57:04 +0000

    second commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
+  * create_file - a function that creates a file
+  * @filename: pointer to file
+  * @text_content: contents 
+  * Return: O
   *
   */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	char buf[1024];
 	int size = sizeof(text_content);
+	char buf[1024];
 
 	fd = open("filename", O_CREAT | O_RDWR | O_APPEND);
 	if (filename == NULL)
 	{
-		write(fd, "", 0);
+		write(fd, "", 1);
 		return (-1);
 	}
 
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	write(STDOUT_FILENO, buf, size);
-	close (fd);
+	close(fd);
 
 	return (0);
 
