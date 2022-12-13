3df2e28aa19d76a0907c1f4fff24980618f6a400
codenamejj <jmtagare@gmail.com>
2022-12-13 09:08:14 +0000

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * main - print arguments passed
+  * @argc: count
+  * @argv: pointer char array
+  * Return: 0
+  */
+int main(int argc, char **argv)
+{
+	printf("Arguments passed to main = %d\n", argc);
+
+	return (0);
+}
