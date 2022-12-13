2c2c18caf552c7434555283af8066cb238bbf21a
codenamejj <jmtagare@gmail.com>
2022-12-13 11:01:56 +0000

    fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,9 +16,12 @@ int main(int argc, char **argv)
 			mul = atoi(argv[1]) * atoi(argv[2]);
 		}
 		printf("%d\n", mul);
+		return (0);
 	}
 	else
+	{
+		printf("Error\n");
 		return (1);
-
+	}
 	return (0);
 }
