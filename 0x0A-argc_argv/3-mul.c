885317dd270108da293f242e5dc0fa637c02bec9
codenamejj <jmtagare@gmail.com>
2022-12-13 10:43:05 +0000

    updated

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * main - multiplies two numbers
+  * @argc: count
+  * @argv: pointer char array
+  * Return: 0
+  */
+int main(int argc, char **argv)
+{
+	int count, mul;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	return (1);
+
+	return (0);
+}
