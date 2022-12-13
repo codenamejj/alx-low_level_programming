ec8ce7785b148746d42c9314654cda2c1f944dd4
codenamejj <jmtagare@gmail.com>
2022-12-13 13:29:43 +0000

    updated

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <ctype.h>
+/**
+  * main - adds positive numbers
+  * @argc: count
+  * @argv: pointer char array
+  * Return: 0
+  */
+int main(int argc, char **argv)
+{
+	int count, sum = 0;
+
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+
+	
+	if (argc > 2)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			while (argv[count])
+			{
+				if (isdigit(atoi(argv[count])) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}	
+			sum = sum + (atoi(argv[count]));
+		}
+		printf("%d\n", sum);
+		return (0);
+	}	
+	return (0);
+}
+		
+
