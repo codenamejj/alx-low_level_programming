a5136211bb8ea7364792b4252437531ff644079a
codenamejj <jmtagare@gmail.com>
2022-12-13 08:48:41 +0000

    header & gitignore file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef _headers_h
+#define _headers_h
+
+/* LIBRARIES */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* PROTOTYPES */
+int _putchar(char c);
+
+#endif
